6b710fca954d54c55947c50f493a5c4a0b8b0c23
Vichy97 <Vincentwilliams97@gmail.com>
2018-05-20 16:51:23 -0700

    add post endpoint for todos

--- server/server.js
+++ server/server.js
@@ -5,4 +5,22 @@ let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
 
+let app = express();
 
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('started on port 3000');
+});
