ad154d1997388668bfde61949cf275a280b608dc
Vichy97 <Vincentwilliams97@gmail.com>
2018-05-20 16:35:29 -0700

    add dependencies

--- server/server.js
+++ server/server.js
@@ -1,23 +1,8 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
 
-let newTodo = new Todo({
-   text: 'do laundry'
-});
 
-newTodo.save().then((doc) => {
-    console.log('model saved', doc);
-}, (err) => {
-    console.log('unable to save model', err);
-});
-
-let newUser = new User({
-    email: 'VincentWilliams97@gmail.com'
-});
-
-newUser.save().then((doc) => {
-    console.log('model saved', doc);
-}, (err) => {
-    console.log('unable to save model', err);
-});
