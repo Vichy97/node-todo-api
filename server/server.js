3a43824974e575b645419c4ee9351cd4fd1733ec
Vichy97 <Vincentwilliams97@gmail.com>
2018-05-22 18:59:59 -0700

    add delete endpoint

--- server/server.js
+++ server/server.js
@@ -33,12 +33,27 @@ app.get('/todos', (req, res) => {
 
 app.get('/todos/:id', (req, res) => {
     let id = req.params.id;
-
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
 
     Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }, (err) => {
+        res.status(400).send();
+    })
+});
+
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
