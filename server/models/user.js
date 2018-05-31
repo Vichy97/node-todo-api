704376291df0e870b6d7aa1507b5f61ed414823c
Vichy97 <Vincentwilliams97@gmail.com>
2018-05-30 20:16:10 -0700

    add POST users endpoint

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,56 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-let User = mongoose.model(('User'), {
+let UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         minLength: 1,
-        trim: true
-    }
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8,
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+UserSchema.methods.toJSON = function () {
+    return _.pick(this.toObject(), ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+    let user = this;
+    let access = 'auth';
+    let token = jwt.sign({_id:user._id.toHexString(), access}, '123abc').toString();
+
+    user.tokens.concat([{
+        access,
+        token,
+    }]);
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+let User = mongoose.model(('User'), UserSchema);
 
 module.exports = {
     User
